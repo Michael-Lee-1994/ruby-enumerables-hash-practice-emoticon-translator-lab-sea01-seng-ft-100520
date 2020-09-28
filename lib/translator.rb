52923c0b5d48c3ad4681c9a50a9b3f3b1ebfbc08
Michael Lee <michael.l33.1994@gmail.com>
2020-09-28 20:24:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon(file_path, emoji)
   emoji = library.keys.find do |key|
     library[key][:english] == emoji
   end
-  emoji ? library[emoji][:japanese] : "Sorry, that emoji was not found"
+  emoji ? library[emoji][:japanese] : "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file_path, emoji)
